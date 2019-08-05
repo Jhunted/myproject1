5af265cd4e88ebae8ccd50dc63cfc16a899012e4
Joseph Hunt <joseph@Josephs-MacBook-Pro.local>
2019-08-05 13:05:03 -0500

    adds function to fill array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
 
+// constants
 
 var suits = ['s', 'c', 'd', 'h'];
 var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
@@ -8,4 +9,15 @@ var warrA = [], player1Hand = [], player2Hand = [];
 var playDeck = '', player2Deck = '', player1Card = '', player2Card = '';
 
 var playing = false;
+
+// functions
+
+function fillArray() {
+    var deck = [];
+    for (var i = 0; i < 52; i++)
+        deck[i] = i;
+
+    shuffle(deck);
+    splitCards(deck);
+}
 
