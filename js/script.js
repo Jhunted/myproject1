0e9e107b7bbe251a592e996605f1e0c894db527b
Joseph Hunt <joseph@Josephs-MacBook-Pro.local>
2019-08-05 16:35:22 -0500

    added starter js for card shuffling

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
-
-
-// constants
-
+/*----- constants -----*/
 var suits = ['s', 'c', 'd', 'h'];
 var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
-var warrA = [], player1Hand = [], player2Hand = [];
-var playDeck = '', player2Deck = '', player1Card = '', player2Card = '';
 
-var playing = false;
+// build a 'master' deck of 'card' objects used to create shuffled decks
+var masterDeck = buildMasterDeck();
+renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
 
-// functions
+/*----- app's state (variables) -----*/
+var shuffledDeck;
 
-function fillArray() {
-    var deck = [];
-    for (var i = 0; i < 52; i++)
-        deck[i] = i;
+/*----- cached element references -----*/
+var shuffledContainer = document.getElementById('shuffled-deck-container');
 
-    shuffle(deck);
-    splitCards(deck);
-}
+/*----- event listeners -----*/
+document.querySelector('button').addEventListener('click', renderShuffledDeck);
 
-function shuffle(deck) {
-    for(var j, x, i = deck.length; i; j = Math.floor(Math.random() * i), x = deck[--i], deck[i] = deck[j], deck[j] = x);
-    return deck;
-}
+/*----- functions -----*/
+function renderShuffledDeck() {
+  // create a copy of the masterDeck (leave masterDeck untouched!)
+  var tempDeck = masterDeck.slice();
+  shuffledDeck = [];
+  while (tempDeck.length) {
+    var rndIdx = Math.floor(Math.random() * tempDeck.length);
+    shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+  }
 
+  
