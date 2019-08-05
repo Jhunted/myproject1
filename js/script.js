d11717f6bc30854e3f0fd0e845a3364662226c78
Joseph Hunt <joseph@Josephs-MacBook-Pro.local>
2019-08-05 16:34:56 -0500

    added starter js for card shuffling

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@
 
 var suits = ['s', 'c', 'd', 'h'];
 var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
-let deck = Array.from(new Array(52), (x, i) => i);
 var warrA = [], player1Hand = [], player2Hand = [];
 var playDeck = '', player2Deck = '', player1Card = '', player2Card = '';
 
@@ -19,5 +18,10 @@ function fillArray() {
 
     shuffle(deck);
     splitCards(deck);
+}
+
+function shuffle(deck) {
+    for(var j, x, i = deck.length; i; j = Math.floor(Math.random() * i), x = deck[--i], deck[i] = deck[j], deck[j] = x);
+    return deck;
 }
 
