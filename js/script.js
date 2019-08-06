95d33431f22638a7de8b40b417874eef2214b177
Joseph Hunt <joseph@Josephs-MacBook-Pro.local>
2019-08-05 21:36:21 -0500

    adds war functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 /*----- constants -----*/
 var suits = ['s', 'c', 'd', 'h'];
 var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
-
+var player1Deck = [];
+var player2Deck = [];
+var player1Card = ('');
+var player2Card = ('');
+var war = [];
 // build a 'master' deck of 'card' objects used to create shuffled decks
 var masterDeck = buildMasterDeck();
 renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
@@ -24,5 +28,48 @@ function renderShuffledDeck() {
     var rndIdx = Math.floor(Math.random() * tempDeck.length);
     shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
   }
+}
 
-  
+function splitCards(deck) {
+    var i = 0;
+    while(i != deck.length) {
+        player1Hand.push(deck[i]);
+        player2Hand.push(deck[(i+1)]);
+        i+=2
+    }
+}
+
+
+
+function compare() {
+   
+    if (player1Card === player2Card)
+    {
+        war();    
+    } 
+    else 
+    {
+        player1Card > player2Card ? player1Deck.push() : player2Deck.push();
+    }
+
+}
+
+function war() {
+    var warA = 4;
+    if (player1Deck.length >= warA && player2Deck.length >= warA)
+        {
+            for(var i = 0; i < 3; i++)
+            {
+                player1Deck.pop();
+            }
+            for(var i = 0; i < 3; i++)
+            {
+                player2Deck.pop();
+            }
+        }
+    if else(player1Deck.length <= warA)
+        {
+            
+        }
+}
+
