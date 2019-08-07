686bf2926cc8050eedb215cb168b44ad382c511c
Joseph Hunt <joseph@Josephs-MacBook-Pro.local>
2019-08-06 19:40:12 -0500

    adds correct comparison and war functionality

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 /*----- constants -----*/
-var suits = ['s', 'c', 'd', 'h'];
-var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+var suits = ['spade', 'club', 'diamond', 'heart'];
+var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14'];
 var player1Deck = [];
 var player2Deck = [];
 var player1Card = ('');
 var player2Card = ('');
 var player1Hand = ('');
 var player2Hand = ('');
-var masterDeck = buildMasterDeck();
 var shuffledDeck;
-var war = [];
-
+var warDeck = [];
+var masterDeck = [];
+var isWar = false;
 
 // build a 'master' deck of 'card' objects used to create shuffled decks
-var masterDeck = buildMasterDeck();
+//var masterDeck = buildMasterDeck();
 //renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
 
 /*----- app's state (variables) -----*/
@@ -27,14 +27,13 @@ document.querySelector('button').addEventListener('click', renderShuffledDeck);
 
 /*----- functions -----*/
 init();
+renderShuffledDeck();
 
 function init() 
     {
-        buildMasterDeck();
-        
+        masterDeck = buildMasterDeck();      
     }
-
-
+   
 function renderShuffledDeck() 
 {
   // create a copy of the masterDeck (leave masterDeck untouched!)
@@ -50,7 +49,7 @@ function renderShuffledDeck()
 }
 
 
-renderShuffledDeck();
+
 
 function buildMasterDeck() {
     var deck = [];
@@ -58,7 +57,9 @@ function buildMasterDeck() {
       ranks.forEach(function(rank) {
         deck.push({
           // the 'face' property maps to the CSS classes for cards
-          face: `${suit}${rank}`,
+         face: `${suit}${rank}`,
+         suit: `${suit}`,
+         rank: `${rank}`
         });
       });
     });
@@ -81,15 +82,50 @@ function splitCards(shuffledDeck)
      console.log('player two has these cards ', player2Deck)
 }
 
-function compare() {
-    if (player1Card === player2Card && (player1Deck.length !== 1 || player2Deck.length !== 1))
+function playCards(deck1, deck2) {
+    // POP to pull cards out, UNSHIFT to push cards in. 
+    player1Card = deck1[0];
+    player2Card = deck2[0];
+    warDeck += (deck1[0], deck2[0]);
+    //console.log(warDeck);
+    player1Deck.shift();
+    player2Deck.shift();
+
+    compare();
+    for (var i = 0; i >= 1; i++)
     {
+        player1Card.push(player1Deck[i]);
+    }
+    // player1Card = player1Deck.pop(); 
+    // player2Card = player2Deck.pop();
+    // pull a card from each players respective decks
+    // call "compareHand function "
+
+
+}
+
+
+function compare() {
+    if (player1Card.rank === player2Card.rank && (player1Deck.length !== 1 || player2Deck.length !== 1) && !isWar)
+    {
+        console.log("***War BITCH***");
         warAction();    
     } 
     else 
     {
+        console.log("***comparing cards***");
         //TODO: give the winner of the comparison the cards in the 'war' array
-        player1Card > player2Card ? (player1Deck.push(player2Card, player1Card)  ): player2Deck.push(player1Card, player2Card);
+        player1Card.rank > player2Card.rank
+        ?
+            (player1Deck.push(warDeck),
+            // console.log(player1Card.face),
+            // console.log(player2Card.face))
+            console.log("player 1: " + player1Card.face+ "\n player 2: " + player2Card.face + "\n player 1 wins!")) 
+        : 
+            (player2Deck.push(warDeck),
+            // console.log(player1Card.face),
+            // console.log(player2Card.face))
+            console.log("player 1: " + player1Card.face + "\n player 2: " + player2Card.face + "\n player 2 wins!"));
         player1Card = ('');
         player2Card = ('');
     }
@@ -98,39 +134,34 @@ function compare() {
 
 // on play round button, begin game 
 
-function playCards(deck1, deck2) {
-    // pull a card from each players respective decks
-
-    // call "compareHand function "
-
-    
-}
 
 
-function compareHand(deck1, deck2) {
+
+//function compareHand(deck1, deck2) {
     // pull P1 & P2's respective decks
     
     // who ever hads the higher card, will win both cards, call "redistribute function "
 
     // in the event of a tie, proceed with "war action" function 
 
-}
+//}
 
 function warAction() {
     var warA = 4;
+    isWar = true;
     //both players have enough cards to play war
-    // if (player1Deck.length >= warA && player2Deck.length >= warA)
-    // {
+    if (player1Deck.length >= warA && player2Deck.length >= warA)
+    {
         for(var i = 0; i < 3; i++)
         {
             war.push(player1Deck.pop());
             war.push(player2Deck.pop());
         }
-        // for(var i = 0; i < 3; i++)
-        // {
-        // }
-        // compare();
-    // }
+        
+        
+        
+        compare();
+    }
     // //player 1 doesn't have enough cards
     // else if (player1Deck.length <= warA && player2Deck.length >= warA)
     // {
@@ -156,19 +187,20 @@ function warAction() {
     //     }
     //     compare();
     // }
+    isWar = false;
 }
 
-function insufficientCardsPop(deck1, deck2)
-{
-        for(var i = 0; i < deck1.length; i++)
-        {
-            war += deck1.pop();
-        }
-        for(var i = 0; i < 3; i++)
-        {
-            war += deck2.pop();
-        }
-}
+// function insufficientCardsPop(deck1, deck2)
+// {
+//         for(var i = 0; i < deck1.length; i++)
+//         {
+//             war += deck1.pop();
+//         }
+//         for(var i = 0; i < 3; i++)
+//         {
+//             war += deck2.pop();
+//         }
+// }
 
 
 
